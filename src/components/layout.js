31748f36197fca8daa9235740d9a0c055797038e
Andrew <dev.andrewhan@gmail.com>
2019-11-13 19:10:50 -0500

    FOOTER ADDED

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { createGlobalStyle } from 'styled-components';
 import Navbar from '../components/globals/navbar/';
+import Footer from '../components/globals/Footer';
 // import './bootstrap.min.css';
 // import './layout.css';
 // import '../sass/layout.scss';
@@ -12,6 +13,7 @@ const Layout = ({ children }) => {
 			<GlobalStyle />
 			<Navbar />
 			{children}
+			<Footer/>
 		</React.Fragment>
 	);
 };
